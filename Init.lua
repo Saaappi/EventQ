72e053ed5505544e93f0b7c6427f2240eecbab4e
Saaappi <alex.metz1024@gmail.com>
2025-06-24 20:00:42 -0700

    Changed my saved variable structure approach.

--- Init.lua
+++ Init.lua
@@ -11,13 +11,58 @@ local eventFrame = CreateFrame("Frame")
   Url: https://wago.tools/db2/LFGDungeons
 ]]
 
+-- Event order:
+-- [1]  = Hallow's End
+-- [2]  = Midsummer
+-- [3]  = Brewfest
+-- [4]  = Love is in the Air
+-- [5]  = TW: TBC
+-- [6]  = TW: WotLK
+-- [7]  = TW: Cata
+-- [8]  = TW: MoP
+-- [9]  = TW: WoD
+-- [10] = TW: Legion
+-- [11] = TW: Classic
+-- [12] = TW: BFA
+
 eventFrame:RegisterEvent("ADDON_LOADED")
 eventFrame:SetScript("OnEvent", function(_, event, addonLoaded)
     if event == "ADDON_LOADED" and addonLoaded == addonName then
         if not EventQDB then
-            EventQDB = {
-                ["Events"] = {
-                    { -- Hallow's End
+            EventQDB = {}
+            EventQDB["Events"] = {}
+        end
+
+        local regionEventIDs = {
+            enUS = { 324, 341, 372, 423, 559, 562, 587, 643, 1056, 1263, 1508, 1669 },
+            enGB = { 324, 341, 372, 423, 616, 622, 628, 652, 1063, 1265, 1583, 1667 },
+            zhCN = { 324, 341, 372, 423, 617, 623, 629, 654, 1068, 1269, 1585, 1666 },
+            enCN = { 324, 341, 372, 423, 617, 623, 629, 654, 1068, 1269, 1585, 1666 },
+            zhTW = { 324, 341, 372, 423, 618, 624, 630, 656, 1065, 1267, 1584, 1668 }
+        }
+
+        local locale = GetLocale()
+        if regionEventIDs[locale] then
+            local regionEvents = regionEventIDs[locale]
+            for _, eventID in ipairs(regionEvents) do
+                if not EventQDB.Events[eventID] then
+                    EventQDB.Events[eventID] = false
+                end
+            end
+        end
+        --if not EventQDB then
+            --EventQDB = {
+                --["Events"] = {
+                    --[[[324] = false, -- Hallow's End
+                    [341] = false, -- Midsummer
+                    [372] = false, -- Brewfest
+                    [423] = false, -- Love is in the Air
+                    [562] = false,
+                    [562] = false,
+                    [562] = false,
+                    [562] = false,
+                    [562] = false,]]
+                    --[[{ -- Hallow's End
                         Enabled = false,
                         EventID = 324,
                         LfgDungeonID = 285,
@@ -29,7 +74,7 @@ eventFrame:SetScript("OnEvent", function(_, event, addonLoaded)
                         LfgDungeonID = 286,
                         TextureID = 368565
                     }
-                    --[[[372] = { -- Brewfest
+                    [372] = { -- Brewfest
                         Enabled = false,
                         LfgDungeonID = 287,
                         TextureID = 132621
@@ -199,8 +244,8 @@ eventFrame:SetScript("OnEvent", function(_, event, addonLoaded)
                         LfgDungeonID = 2874,
                         TextureID = 2065640
                     },]]
-                }
-            }
-        end
+                --}
+            --}
+        --end
     end
 end)
