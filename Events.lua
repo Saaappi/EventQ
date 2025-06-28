c33e73614b697444f7f873fde56f5d58460a28ad
Saaappi <alex.metz1024@gmail.com>
2025-06-27 18:51:44 -0700

    Localized all the event names.

--- Events.lua
+++ Events.lua
@@ -23,40 +23,40 @@ addonTable.RegionEventIDs = {
 
 -- Localize the names
 addonTable.Events = {
-    [324] = { LfgDungeonID = 285, TextureID = 133661, Name = "Hallow's End" },
-    [341] = { LfgDungeonID = 286, TextureID = 368565, Name = "Midsummer Fire Festival" },
-    [372] = { LfgDungeonID = 287, TextureID = 132621, Name = "Brewfest" },
-    [423] = { LfgDungeonID = 288, TextureID = 135450, Name = "Love is in the Air" },
-    [559] = { LfgDungeonID = 744, TextureID = 630783, Name = "Timewalking: The Burning Crusade" }, -- US
-    [562] = { LfgDungeonID = 995, TextureID = 630787, Name = "Timewalking: Wrath of the Lich King" }, -- US
-    [587] = { LfgDungeonID = 1146, TextureID = 630784, Name = "Timewalking: Cataclysm" }, -- US
-    [616] = { LfgDungeonID = 995, TextureID = 630787, Name = "Timewalking: Wrath of the Lich King" }, -- EU
-    [617] = { LfgDungeonID = 995, TextureID = 630787, Name = "Timewalking: Wrath of the Lich King" }, -- CN/KR
-    [618] = { LfgDungeonID = 744, TextureID = 630783, Name = "Timewalking: The Burning Crusade" }, -- TW
-    [622] = { LfgDungeonID = 744, TextureID = 630783, Name = "Timewalking: The Burning Crusade" }, -- EU
-    [623] = { LfgDungeonID = 744, TextureID = 630783, Name = "Timewalking: The Burning Crusade" }, -- CN/KR
-    [624] = { LfgDungeonID = 995, TextureID = 630787, Name = "Timewalking: Wrath of the Lich King" }, -- TW
-    [628] = { LfgDungeonID = 1146, TextureID = 630784, Name = "Timewalking: Cataclysm" }, -- EU
-    [629] = { LfgDungeonID = 1146, TextureID = 630784, Name = "Timewalking: Cataclysm" }, -- CN/KR
-    [630] = { LfgDungeonID = 1146, TextureID = 630784, Name = "Timewalking: Cataclysm" }, -- TW
-    [643] = { LfgDungeonID = 1453, TextureID = 630786, Name = "Timewalking: Mists of Pandaria" }, -- US
-    [652] = { LfgDungeonID = 1453, TextureID = 630786, Name = "Timewalking: Mists of Pandaria" }, -- EU
-    [654] = { LfgDungeonID = 1453, TextureID = 630786, Name = "Timewalking: Mists of Pandaria" }, -- CN/KR
-    [656] = { LfgDungeonID = 1453, TextureID = 630786, Name = "Timewalking: Mists of Pandaria" }, -- TW
-    [1056] = { LfgDungeonID = 1971, TextureID = 2838050, Name = "Timewalking: Warlords of Draenor" }, -- US
-    [1063] = { LfgDungeonID = 1971, TextureID = 2838050, Name = "Timewalking: Warlords of Draenor" }, -- EU
-    [1065] = { LfgDungeonID = 1971, TextureID = 2838050, Name = "Timewalking: Warlords of Draenor" }, -- TW
-    [1068] = { LfgDungeonID = 1971, TextureID = 2838050, Name = "Timewalking: Warlords of Draenor" }, -- CN/KR
-    [1263] = { LfgDungeonID = 2274, TextureID = 1408999, Name = "Timewalking: Legion" }, -- US
-    [1265] = { LfgDungeonID = 2274, TextureID = 1408999, Name = "Timewalking: Legion" }, -- EU
-    [1267] = { LfgDungeonID = 2274, TextureID = 1408999, Name = "Timewalking: Legion" }, -- TW
-    [1269] = { LfgDungeonID = 2274, TextureID = 1408999, Name = "Timewalking: Legion" }, -- CN/KR
-    [1508] = { LfgDungeonID = 2634, TextureID = 236761, Name = "Timewalking: Classic" }, -- US
-    [1583] = { LfgDungeonID = 2634, TextureID = 236761, Name = "Timewalking: Classic" }, -- EU
-    [1584] = { LfgDungeonID = 2634, TextureID = 236761, Name = "Timewalking: Classic" }, -- TW
-    [1585] = { LfgDungeonID = 2634, TextureID = 236761, Name = "Timewalking: Classic" }, -- CN/KR
-    [1666] = { LfgDungeonID = 2874, TextureID = 2065640, Name = "Timewalking: Battle for Azeroth" }, -- TW
-    [1667] = { LfgDungeonID = 2874, TextureID = 2065640, Name = "Timewalking: Battle for Azeroth" }, -- EU
-    [1668] = { LfgDungeonID = 2874, TextureID = 2065640, Name = "Timewalking: Battle for Azeroth" }, -- CN/KR
-    [1669] = { LfgDungeonID = 2874, TextureID = 2065640, Name = "Timewalking: Battle for Azeroth" }, -- US
+    [324] = { LfgDungeonID = 285, TextureID = 133661, Name = addonTable.Locales.HALLOWS_END },
+    [341] = { LfgDungeonID = 286, TextureID = 368565, Name = addonTable.Locales.MIDSUMMER },
+    [372] = { LfgDungeonID = 287, TextureID = 132621, Name = addonTable.Locales.BREWFEST },
+    [423] = { LfgDungeonID = 288, TextureID = 135450, Name = addonTable.Locales.LOVE_IS_IN_THE_AIR },
+    [559] = { LfgDungeonID = 744, TextureID = 630783, Name = addonTable.Locales.TIMEWALKING_TBC }, -- US
+    [562] = { LfgDungeonID = 995, TextureID = 630787, Name = addonTable.Locales.TIMEWALKING_WOTLK }, -- US
+    [587] = { LfgDungeonID = 1146, TextureID = 630784, Name = addonTable.Locales.TIMEWALKING_CATA }, -- US
+    [616] = { LfgDungeonID = 995, TextureID = 630787, Name = addonTable.Locales.TIMEWALKING_WOTLK }, -- EU
+    [617] = { LfgDungeonID = 995, TextureID = 630787, Name = addonTable.Locales.TIMEWALKING_WOTLK }, -- CN/KR
+    [618] = { LfgDungeonID = 744, TextureID = 630783, Name = addonTable.Locales.TIMEWALKING_TBC }, -- TW
+    [622] = { LfgDungeonID = 744, TextureID = 630783, Name = addonTable.Locales.TIMEWALKING_TBC }, -- EU
+    [623] = { LfgDungeonID = 744, TextureID = 630783, Name = addonTable.Locales.TIMEWALKING_TBC }, -- CN/KR
+    [624] = { LfgDungeonID = 995, TextureID = 630787, Name = addonTable.Locales.TIMEWALKING_WOTLK }, -- TW
+    [628] = { LfgDungeonID = 1146, TextureID = 630784, Name = addonTable.Locales.TIMEWALKING_CATA }, -- EU
+    [629] = { LfgDungeonID = 1146, TextureID = 630784, Name = addonTable.Locales.TIMEWALKING_CATA }, -- CN/KR
+    [630] = { LfgDungeonID = 1146, TextureID = 630784, Name = addonTable.Locales.TIMEWALKING_CATA }, -- TW
+    [643] = { LfgDungeonID = 1453, TextureID = 630786, Name = addonTable.Locales.TIMEWALKING_MOP }, -- US
+    [652] = { LfgDungeonID = 1453, TextureID = 630786, Name = addonTable.Locales.TIMEWALKING_MOP }, -- EU
+    [654] = { LfgDungeonID = 1453, TextureID = 630786, Name = addonTable.Locales.TIMEWALKING_MOP }, -- CN/KR
+    [656] = { LfgDungeonID = 1453, TextureID = 630786, Name = addonTable.Locales.TIMEWALKING_MOP }, -- TW
+    [1056] = { LfgDungeonID = 1971, TextureID = 2838050, Name = addonTable.Locales.TIMEWALKING_WOD }, -- US
+    [1063] = { LfgDungeonID = 1971, TextureID = 2838050, Name = addonTable.Locales.TIMEWALKING_WOD }, -- EU
+    [1065] = { LfgDungeonID = 1971, TextureID = 2838050, Name = addonTable.Locales.TIMEWALKING_WOD }, -- TW
+    [1068] = { LfgDungeonID = 1971, TextureID = 2838050, Name = addonTable.Locales.TIMEWALKING_WOD }, -- CN/KR
+    [1263] = { LfgDungeonID = 2274, TextureID = 1408999, Name = addonTable.Locales.TIMEWALKING_LEGION }, -- US
+    [1265] = { LfgDungeonID = 2274, TextureID = 1408999, Name = addonTable.Locales.TIMEWALKING_LEGION }, -- EU
+    [1267] = { LfgDungeonID = 2274, TextureID = 1408999, Name = addonTable.Locales.TIMEWALKING_LEGION }, -- TW
+    [1269] = { LfgDungeonID = 2274, TextureID = 1408999, Name = addonTable.Locales.TIMEWALKING_LEGION }, -- CN/KR
+    [1508] = { LfgDungeonID = 2634, TextureID = 236761, Name = addonTable.Locales.TIMEWALKING_CLASSIC }, -- US
+    [1583] = { LfgDungeonID = 2634, TextureID = 236761, Name = addonTable.Locales.TIMEWALKING_CLASSIC }, -- EU
+    [1584] = { LfgDungeonID = 2634, TextureID = 236761, Name = addonTable.Locales.TIMEWALKING_CLASSIC }, -- TW
+    [1585] = { LfgDungeonID = 2634, TextureID = 236761, Name = addonTable.Locales.TIMEWALKING_CLASSIC }, -- CN/KR
+    [1666] = { LfgDungeonID = 2874, TextureID = 2065640, Name = addonTable.Locales.TIMEWALKING_BFA }, -- TW
+    [1667] = { LfgDungeonID = 2874, TextureID = 2065640, Name = addonTable.Locales.TIMEWALKING_BFA }, -- EU
+    [1668] = { LfgDungeonID = 2874, TextureID = 2065640, Name = addonTable.Locales.TIMEWALKING_BFA }, -- CN/KR
+    [1669] = { LfgDungeonID = 2874, TextureID = 2065640, Name = addonTable.Locales.TIMEWALKING_BFA }, -- US
 }
--- Locales.lua
+++ Locales.lua
@@ -26,6 +26,19 @@ L["ENABLED_DESC"]   = "Toggle to show or hide the queue button."
 L["AUTO_ENROLLMENT"]        = "Auto Enrollment"
 L["AUTO_ENROLLMENT_DESC"]   = "Toggle to automatically enroll in events based on whether or not they're active."
 
+L["BREWFEST"]               = "Brewfest"
+L["LOVE_IS_IN_THE_AIR"]     = "Love is in the Air"
+L["HALLOWS_END"]            = "Hallow's End"
+L["MIDSUMMER"]              = "Midsummer Fire Festival"
+L["TIMEWALKING_CLASSIC"]    = "Timewalking: " .. EXPANSION_NAME0
+L["TIMEWALKING_TBC"]        = "Timewalking: " .. EXPANSION_NAME1
+L["TIMEWALKING_WOTLK"]      = "Timewalking: " .. EXPANSION_NAME2
+L["TIMEWALKING_CATA"]       = "Timewalking: " .. EXPANSION_NAME3
+L["TIMEWALKING_MOP"]        = "Timewalking: " .. EXPANSION_NAME4
+L["TIMEWALKING_WOD"]        = "Timewalking: " .. EXPANSION_NAME5
+L["TIMEWALKING_LEGION"]     = "Timewalking: " .. EXPANSION_NAME6
+L["TIMEWALKING_BFA"]        = "Timewalking: " .. EXPANSION_NAME7
+
 local L = Locales.enGB
 
 L["EVENTQ"]         = "eventq"
@@ -36,7 +49,22 @@ L["ENABLED_DESC"]   = "Toggle to show or hide the queue button."
 L["AUTO_ENROLLMENT"]        = "Auto Enrollment"
 L["AUTO_ENROLLMENT_DESC"]   = "Toggle to automatically enroll in events based on whether or not they're active."
 
+L["BREWFEST"]               = "Brewfest"
+L["LOVE_IS_IN_THE_AIR"]     = "Love is in the Air"
+L["HALLOWS_END"]            = "Hallow's End"
+L["MIDSUMMER"]              = "Midsummer Fire Festival"
+L["TIMEWALKING_CLASSIC"]    = "Timewalking: " .. EXPANSION_NAME0
+L["TIMEWALKING_TBC"]        = "Timewalking: " .. EXPANSION_NAME1
+L["TIMEWALKING_WOTLK"]      = "Timewalking: " .. EXPANSION_NAME2
+L["TIMEWALKING_CATA"]       = "Timewalking: " .. EXPANSION_NAME3
+L["TIMEWALKING_MOP"]        = "Timewalking: " .. EXPANSION_NAME4
+L["TIMEWALKING_WOD"]        = "Timewalking: " .. EXPANSION_NAME5
+L["TIMEWALKING_LEGION"]     = "Timewalking: " .. EXPANSION_NAME6
+L["TIMEWALKING_BFA"]        = "Timewalking: " .. EXPANSION_NAME7
+
 local L = Locales.deDE
+
+
 local L = Locales.esES
 local L = Locales.esMX
 local L = Locales.frFR
